83104935e27631d4e07efcfb8bb341f0ba2f0cb0
tonxycs <104640218+tonxycs@users.noreply.github.com>
2026-08-07 00:11:29 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -200,7 +200,8 @@ def cal_kappa(hist):
             kappa = (po - pe) / (1 - pe)
     return kappa
 
-ef SCDD_eval_all(preds, labels, num_class):
+# sek37-SECOND/sek17-Landsat-SCD/sek26-HRSCD
+def SCDD_eval_all(preds, labels, num_class):
     hist = np.zeros((num_class, num_class))
     for pred, label in zip(preds, labels):
         infer_array = np.array(pred)
@@ -235,36 +236,36 @@ ef SCDD_eval_all(preds, labels, num_class):
     return Fscd, IoU_mean, Sek
 
 
-def SCDD_eval(pred, label, num_class):
-    infer_array = np.array(pred)
-    unique_set = set(np.unique(infer_array))
-    assert unique_set.issubset(set([0, 1, 2, 3, 4, 5, 6])), "unrecognized label number"
-    label_array = np.array(label)
-    assert infer_array.shape == label_array.shape, "The size of prediction and target must be the same"
-    hist = get_hist(infer_array, label_array, num_class)
-    hist_fg = hist[1:, 1:]
-    c2hist = np.zeros((2, 2))
-    c2hist[0][0] = hist[0][0]
-    c2hist[0][1] = hist.sum(1)[0] - hist[0][0]
-    c2hist[1][0] = hist.sum(0)[0] - hist[0][0]
-    c2hist[1][1] = hist_fg.sum()
-    hist_n0 = hist.copy()
-    hist_n0[0][0] = 0
-    kappa_n0 = cal_kappa(hist_n0)
-    iu = np.diag(c2hist) / (c2hist.sum(1) + c2hist.sum(0) - np.diag(c2hist))
-    IoU_fg = iu[1]
-    IoU_mean = (iu[0] + iu[1]) / 2
-    Sek = (kappa_n0 * math.exp(IoU_fg)) / math.e
+# def SCDD_eval(pred, label, num_class):
+#     infer_array = np.array(pred)
+#     unique_set = set(np.unique(infer_array))
+#     assert unique_set.issubset(set([0, 1, 2, 3, 4, 5, 6])), "unrecognized label number"
+#     label_array = np.array(label)
+#     assert infer_array.shape == label_array.shape, "The size of prediction and target must be the same"
+#     hist = get_hist(infer_array, label_array, num_class)
+#     hist_fg = hist[1:, 1:]
+#     c2hist = np.zeros((2, 2))
+#     c2hist[0][0] = hist[0][0]
+#     c2hist[0][1] = hist.sum(1)[0] - hist[0][0]
+#     c2hist[1][0] = hist.sum(0)[0] - hist[0][0]
+#     c2hist[1][1] = hist_fg.sum()
+#     hist_n0 = hist.copy()
+#     hist_n0[0][0] = 0
+#     kappa_n0 = cal_kappa(hist_n0)
+#     iu = np.diag(c2hist) / (c2hist.sum(1) + c2hist.sum(0) - np.diag(c2hist))
+#     IoU_fg = iu[1]
+#     IoU_mean = (iu[0] + iu[1]) / 2
+#     Sek = (kappa_n0 * math.exp(IoU_fg)) / math.e
         
-    pixel_sum = hist.sum()
-    change_pred_sum  = pixel_sum - hist.sum(1)[0].sum()
-    change_label_sum = pixel_sum - hist.sum(0)[0].sum()
-    change_ratio = change_label_sum/pixel_sum
-    SC_TP = np.diag(hist[1:, 1:]).sum()
-    SC_Precision = SC_TP/change_pred_sum
-    SC_Recall = SC_TP/change_label_sum
-    Fscd = stats.hmean([SC_Precision, SC_Recall])
-    return Fscd, IoU_mean, Sek
+#     pixel_sum = hist.sum()
+#     change_pred_sum  = pixel_sum - hist.sum(1)[0].sum()
+#     change_label_sum = pixel_sum - hist.sum(0)[0].sum()
+#     change_ratio = change_label_sum/pixel_sum
+#     SC_TP = np.diag(hist[1:, 1:]).sum()
+#     SC_Precision = SC_TP/change_pred_sum
+#     SC_Recall = SC_TP/change_label_sum
+#     Fscd = stats.hmean([SC_Precision, SC_Recall])
+#     return Fscd, IoU_mean, Sek
 
 def FWIoU(pred, label, bn_mode=False, ignore_zero=False):
     if bn_mode:
