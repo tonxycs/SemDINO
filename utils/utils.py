222ef37ed76423463948c5ce5d6c3997674e1446
tonxycs <104640218+tonxycs@users.noreply.github.com>
2026-08-07 00:32:07 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -176,15 +176,18 @@ def accuracy(pred, label, ignore_zero=False):
     valid_sum = valid.sum()
     acc = float(acc_sum) / (valid_sum + 1e-10)
     return acc, valid_sum
-    
+
+
 def fast_hist(a, b, n):
     k = (a >= 0) & (a < n)
     return np.bincount(n * a[k].astype(int) + b[k], minlength=n ** 2).reshape(n, n)
+
 
 def get_hist(image, label, num_class):
     hist = np.zeros((num_class, num_class))
     hist += fast_hist(image.flatten(), label.flatten(), num_class)
     return hist
+
 
 def cal_kappa(hist):
     if hist.sum() == 0:
@@ -200,80 +203,17 @@ def cal_kappa(hist):
             kappa = (po - pe) / (1 - pe)
     return kappa
 
-# # sek37-SECOND/sek17-Landsat-SCD/sek26-HRSCD
-# def SCDD_eval_all(preds, labels, num_class):
-#     hist = np.zeros((num_class, num_class))
-#     for pred, label in zip(preds, labels):
-#         infer_array = np.array(pred)
-#         label_array = np.array(label)
-#         unique_set = set(np.unique(infer_array))
-#         assert unique_set.issubset(set(range(num_class))), "unrecognized label number"
-#         assert infer_array.shape == label_array.shape, "The size of prediction and target must be the same"
-#         hist += get_hist(infer_array, label_array, num_class)
-
-#     hist_fg = hist[1:, 1:]
-#     c2hist = np.zeros((2, 2))
-#     c2hist[0][0] = hist[0][0]
-#     c2hist[0][1] = hist.sum(1)[0] - hist[0][0]
-#     c2hist[1][0] = hist.sum(0)[0] - hist[0][0]
-#     c2hist[1][1] = hist_fg.sum()
-
-#     hist_n0 = hist.copy()
-#     hist_n0[0][0] = 0
-#     kappa_n0 = cal_kappa(hist_n0)
-#     iu = np.diag(c2hist) / (c2hist.sum(1) + c2hist.sum(0) - np.diag(c2hist) + 1e-10)
-#     iou_fg = iu[1]
-#     IoU_mean = (iu[0] + iu[1]) / 2
-#     Sek = (kappa_n0 * math.exp(iou_fg)) / math.e
-
-#     pixel_sum = hist.sum()
-#     change_pred_sum = pixel_sum - hist.sum(1)[0].sum()
-#     change_label_sum = pixel_sum - hist.sum(0)[0].sum()
-#     sc_tp = np.diag(hist[1:, 1:]).sum()
-#     sc_precision = sc_tp / (change_pred_sum + 1e-10)
-#     sc_recall = sc_tp / (change_label_sum + 1e-10)
-#     Fscd = stats.hmean([sc_precision, sc_recall]) if sc_precision > 0 and sc_recall > 0 else 0.0
-#     return Fscd, IoU_mean, Sek
-
-
-# def SCDD_eval(pred, label, num_class):
-#     infer_array = np.array(pred)
-#     unique_set = set(np.unique(infer_array))
-#     assert unique_set.issubset(set([0, 1, 2, 3, 4, 5, 6])), "unrecognized label number"
-#     label_array = np.array(label)
-#     assert infer_array.shape == label_array.shape, "The size of prediction and target must be the same"
-#     hist = get_hist(infer_array, label_array, num_class)
-#     hist_fg = hist[1:, 1:]
-#     c2hist = np.zeros((2, 2))
-#     c2hist[0][0] = hist[0][0]
-#     c2hist[0][1] = hist.sum(1)[0] - hist[0][0]
-#     c2hist[1][0] = hist.sum(0)[0] - hist[0][0]
-#     c2hist[1][1] = hist_fg.sum()
-#     hist_n0 = hist.copy()
-#     hist_n0[0][0] = 0
-#     kappa_n0 = cal_kappa(hist_n0)
-#     iu = np.diag(c2hist) / (c2hist.sum(1) + c2hist.sum(0) - np.diag(c2hist))
-#     IoU_fg = iu[1]
-#     IoU_mean = (iu[0] + iu[1]) / 2
-#     Sek = (kappa_n0 * math.exp(IoU_fg)) / math.e
-        
-#     pixel_sum = hist.sum()
-#     change_pred_sum  = pixel_sum - hist.sum(1)[0].sum()
-#     change_label_sum = pixel_sum - hist.sum(0)[0].sum()
-#     change_ratio = change_label_sum/pixel_sum
-#     SC_TP = np.diag(hist[1:, 1:]).sum()
-#     SC_Precision = SC_TP/change_pred_sum
-#     SC_Recall = SC_TP/change_label_sum
-#     Fscd = stats.hmean([SC_Precision, SC_Recall])
-#     return Fscd, IoU_mean, Sek
 
 def SCDD_eval_all(preds, labels, num_class):
     hist = np.zeros((num_class, num_class))
     for pred, label in zip(preds, labels):
         infer_array = np.array(pred)
-        label_array = np.array(label)
         unique_set = set(np.unique(infer_array))
-        assert unique_set.issubset(set(range(num_class))), "unrecognized label number"
+        # import pdb; pdb.set_trace()
+        #assert unique_set.issubset(set([x for x in range(num_class)])), "unrecognized label number"
+        # if not unique_set.issubset(set([x for x in range(num_class)])):
+        #     print(unique_set)
+        label_array = np.array(label)
         assert infer_array.shape == label_array.shape, "The size of prediction and target must be the same"
         hist += get_hist(infer_array, label_array, num_class)
 
@@ -283,63 +223,100 @@ def SCDD_eval_all(preds, labels, num_class):
     c2hist[0][1] = hist.sum(1)[0] - hist[0][0]
     c2hist[1][0] = hist.sum(0)[0] - hist[0][0]
     c2hist[1][1] = hist_fg.sum()
-
     hist_n0 = hist.copy()
     hist_n0[0][0] = 0
     kappa_n0 = cal_kappa(hist_n0)
-    iu = np.diag(c2hist) / (c2hist.sum(1) + c2hist.sum(0) - np.diag(c2hist) + 1e-10)
-    iou_fg = iu[1]
-    iou_mean = (iu[0] + iu[1]) / 2
-    sek = (kappa_n0 * math.exp(iou_fg)) / math.e
+    iu = np.diag(c2hist) / (c2hist.sum(1) + c2hist.sum(0) - np.diag(c2hist))
+    IoU_fg = iu[1]
+    IoU_mean = (iu[0] + iu[1]) / 2
+    Sek = (kappa_n0 * math.exp(IoU_fg)) / math.e
 
     pixel_sum = hist.sum()
     change_pred_sum = pixel_sum - hist.sum(1)[0].sum()
     change_label_sum = pixel_sum - hist.sum(0)[0].sum()
-    sc_tp = np.diag(hist[1:, 1:]).sum()
-    sc_precision = sc_tp / (change_pred_sum + 1e-10)
-    sc_recall = sc_tp / (change_label_sum + 1e-10)
-    fscd = stats.hmean([sc_precision, sc_recall]) if sc_precision > 0 and sc_recall > 0 else 0.0
-    return float(kappa_n0), float(fscd), float(iou_mean), float(sek)
+    change_ratio = change_label_sum / pixel_sum
+    SC_TP = np.diag(hist[1:, 1:]).sum()
+    SC_Precision = SC_TP / change_pred_sum
+    SC_Recall = SC_TP / change_label_sum
+    Fscd = stats.hmean([SC_Precision, SC_Recall])
+    return kappa_n0, Fscd, IoU_mean, Sek
 
+
+def SCDD_eval_single_sample(pred, label, num_class):
+    hist = np.zeros((num_class, num_class))
+    # for pred, label in zip(preds, labels):
+    infer_array = np.array(pred)
+    unique_set = set(np.unique(infer_array))
+    label_array = np.array(label)
+    assert infer_array.shape == label_array.shape, "The size of prediction and target must be the same"
+    hist += get_hist(infer_array, label_array, num_class)
+
+    hist_fg = hist[1:, 1:]
+    c2hist = np.zeros((2, 2))
+    c2hist[0][0] = hist[0][0]
+    c2hist[0][1] = hist.sum(1)[0] - hist[0][0]
+    c2hist[1][0] = hist.sum(0)[0] - hist[0][0]
+    c2hist[1][1] = hist_fg.sum()
+    hist_n0 = hist.copy()
+    hist_n0[0][0] = 0
+    kappa_n0 = cal_kappa(hist_n0)
+    iu = np.diag(c2hist) / (c2hist.sum(1) + c2hist.sum(0) - np.diag(c2hist))
+    IoU_fg = iu[1]
+    IoU_mean = (iu[0] + iu[1]) / 2
+    Sek = (kappa_n0 * math.exp(IoU_fg)) / math.e
+
+    pixel_sum = hist.sum()
+    change_pred_sum = pixel_sum - hist.sum(1)[0].sum()
+    change_label_sum = pixel_sum - hist.sum(0)[0].sum()
+    change_ratio = change_label_sum / pixel_sum
+    SC_TP = np.diag(hist[1:, 1:]).sum()
+    SC_Precision = SC_TP / change_pred_sum
+    SC_Recall = SC_TP / change_label_sum
+    Fscd = stats.hmean([SC_Precision, SC_Recall])
+    return kappa_n0, Fscd, IoU_mean, Sek
 
 def SCDD_eval(pred, label, num_class):
-    return SCDD_eval_all([pred], [label], num_class)[1:]
+    infer_array = np.array(pred)
+    unique_set = set(np.unique(infer_array))
+    assert unique_set.issubset(set([0, 1, 2, 3, 4, 5, 6])), "unrecognized label number"
+    label_array = np.array(label)
+    assert infer_array.shape == label_array.shape, "The size of prediction and target must be the same"
+    hist = get_hist(infer_array, label_array, num_class)
+    hist_fg = hist[1:, 1:]
+    c2hist = np.zeros((2, 2))
+    c2hist[0][0] = hist[0][0]
+    c2hist[0][1] = hist.sum(1)[0] - hist[0][0]
+    c2hist[1][0] = hist.sum(0)[0] - hist[0][0]
+    c2hist[1][1] = hist_fg.sum()
+    hist_n0 = hist.copy()
+    hist_n0[0][0] = 0
+    kappa_n0 = cal_kappa(hist_n0)
+    iu = np.diag(c2hist) / (c2hist.sum(1) + c2hist.sum(0) - np.diag(c2hist))
+    IoU_fg = iu[1]
+    IoU_mean = (iu[0] + iu[1]) / 2
+    Sek = (kappa_n0 * math.exp(IoU_fg)) / math.e
+
+    pixel_sum = hist.sum()
+    change_pred_sum = pixel_sum - hist.sum(1)[0].sum()
+    change_label_sum = pixel_sum - hist.sum(0)[0].sum()
+    change_ratio = change_label_sum / pixel_sum
+    SC_TP = np.diag(hist[1:, 1:]).sum()
+    SC_Precision = SC_TP / change_pred_sum
+    SC_Recall = SC_TP / change_label_sum
+    Fscd = stats.hmean([SC_Precision, SC_Recall])
+    return Fscd, IoU_mean, Sek
+
 
 def FWIoU(pred, label, bn_mode=False, ignore_zero=False):
     if bn_mode:
-        pred = (pred>= 0.5)
-        label = (label>= 0.5)
+        pred = (pred >= 0.5)
+        label = (label >= 0.5)
     elif ignore_zero:
-        pred = pred-1
-        label = label-1
+        pred = pred - 1
+        label = label - 1
     FWIoU = seg_acc.frequency_weighted_IU(pred, label)
     return FWIoU
 
-class SemanticChangeEvaluator:
-    def __init__(self, num_class=37):
-        self.num_class = num_class
-        self.reset()
-
-    def reset(self):
-        self.preds = []
-        self.labels = []
-        self.acc_meter = AverageMeter()
-
-    def add_batch(self, pred_scd, label_scd):
-        acc, _ = accuracy(pred_scd, label_scd)
-        self.preds.append(np.asarray(pred_scd))
-        self.labels.append(np.asarray(label_scd))
-        self.acc_meter.update(acc)
-
-    def compute(self):
-        kappa_n0, fscd, iou_mean, sek = SCDD_eval_all(self.preds, self.labels, self.num_class)
-        return SemanticChangeMetrics(
-            kappa=float(kappa_n0),
-            fscd=float(fscd),
-            miou=float(iou_mean),
-            sek=float(sek),
-            oa=float(self.acc_meter.avg),
-        )
 
 def binary_accuracy(pred, label):
     valid = (label < 2)
@@ -347,6 +324,7 @@ def binary_accuracy(pred, label):
     valid_sum = valid.sum()
     acc = float(acc_sum) / (valid_sum + 1e-10)
     return acc
+
 
 def intersectionAndUnion(imPred, imLab, numClass):
     imPred = np.asarray(imPred).copy()
@@ -361,17 +339,18 @@ def intersectionAndUnion(imPred, imLab, numClass):
     # Compute area intersection:
     intersection = imPred * (imPred == imLab)
     (area_intersection, _) = np.histogram(
-        intersection, bins=numClass, range=(1, numClass+1))
+        intersection, bins=numClass, range=(1, numClass + 1))
     # print(area_intersection)
 
     # Compute area union:
-    (area_pred, _) = np.histogram(imPred, bins=numClass, range=(1, numClass+1))
-    (area_lab, _) = np.histogram(imLab, bins=numClass, range=(1, numClass+1))
+    (area_pred, _) = np.histogram(imPred, bins=numClass, range=(1, numClass + 1))
+    (area_lab, _) = np.histogram(imLab, bins=numClass, range=(1, numClass + 1))
     area_union = area_pred + area_lab - area_intersection
     # print(area_pred)
     # print(area_lab)
 
     return (area_intersection, area_union)
+
 
 def CaclTP(imPred, imLab, numClass):
     imPred = np.asarray(imPred).copy()
@@ -386,13 +365,13 @@ def CaclTP(imPred, imLab, numClass):
     # Compute area intersection:
     TP = imPred * (imPred == imLab)
     (TP_hist, _) = np.histogram(
-        TP, bins=numClass, range=(1, numClass+1))
+        TP, bins=numClass, range=(1, numClass + 1))
     # print(TP.shape)
     # print(TP_hist)
 
     # Compute area union:
-    (pred_hist, _) = np.histogram(imPred, bins=numClass, range=(1, numClass+1))
-    (lab_hist, _) = np.histogram(imLab, bins=numClass, range=(1, numClass+1))
+    (pred_hist, _) = np.histogram(imPred, bins=numClass, range=(1, numClass + 1))
+    (lab_hist, _) = np.histogram(imLab, bins=numClass, range=(1, numClass + 1))
     # print(pred_hist)
     # print(lab_hist)
     # precision = TP_hist / (lab_hist + 1e-10) + 1e-10
@@ -402,8 +381,9 @@ def CaclTP(imPred, imLab, numClass):
     # F1 = [stats.hmean([pre, rec]) for pre, rec in zip(precision, recall)]
     # print(F1)
 
-
     # print(area_pred)
     # print(area_lab)
+
+    return (TP_hist, pred_hist, lab_hist)
 
     return (TP_hist, pred_hist, lab_hist)
